e05fe449c11ac9f06b5a6112c3f79436d95b7ede
brunosimm <bruno.simm@edu.pucrs.br>
2021-11-10 20:28:33 -0300

    Initial project

---
+++ doe/src/test/java/com/pds/doe/DoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pds.doe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
