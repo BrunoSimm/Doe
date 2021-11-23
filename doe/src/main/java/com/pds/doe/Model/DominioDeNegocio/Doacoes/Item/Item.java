c9e056e693135def2c2bca243df6ff523ba64b9a
MiguelZanela <57719624+MiguelZanela@users.noreply.github.com>
2021-11-23 09:05:44 -0300

    [Fix] itens e necessidades

--- doe/src/main/java/com/pds/doe/Model/DominioDeNegocio/Doacoes/Item/Item.java
+++ doe/src/main/java/com/pds/doe/Model/DominioDeNegocio/Doacoes/Item/Item.java
@@ -3,6 +3,47 @@ package com.pds.doe.Model.DominioDeNegocio.Doacoes.Item;
 public class Item {
     private String nome;
     private String descricao;
-    private String imagem;
+    private int imagemId;
     private Boolean ativo;
+
+
+    public Item(String nome, String descricao, int imagemId, Boolean ativo) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.imagemId = imagemId;
+        this.ativo = ativo;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getImagemId() {
+        return this.imagemId;
+    }
+
+    public void setImagemId(int imagemId) {
+        this.imagemId = imagemId;
+    }
+    
+    public Boolean getAtivo() {
+        return this.ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+
 }
--- doe/src/main/java/com/pds/doe/Model/DominioDeNegocio/Doacoes/Necessidade/Necessidade.java
+++ doe/src/main/java/com/pds/doe/Model/DominioDeNegocio/Doacoes/Necessidade/Necessidade.java
@@ -5,4 +5,35 @@ import java.util.Date;
 public class Necessidade {
     private int quantidade;
     private Date prazoLimite;
+    private int itemId;
+
+    public Necessidade(int quantidade, Date prazoLimite, int itemId) {
+        this.quantidade = quantidade;
+        this.prazoLimite = prazoLimite;
+        this.itemId = itemId;
+    }
+
+    public int getQuantidade() {
+        return this.quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Date getPrazoLimite() {
+        return this.prazoLimite;
+    }
+
+    public void setPrazoLimite(Date prazoLimite) {
+        this.prazoLimite = prazoLimite;
+    }
+
+    public int getItemId() {
+        return this.itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
 }
