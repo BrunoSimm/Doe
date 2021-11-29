fce2c456f1e6d2ed28e0d8e1327acb29925ec924
MiguelZanela <57719624+MiguelZanela@users.noreply.github.com>
2021-11-29 18:20:57 -0300

    [Cut] extra folders

--- doe/src/main/java/com/pds/doe/Model/Repositorios/ItensRepositorio/ItensPersistencia.java
+++
@@ -1,7 +0,0 @@
-package com.pds.doe.Model.Repositorios.ItensRepositorio;
-
-import com.pds.doe.Model.Repositorios.DoacoesRepositorio.ItensNecessidade;
-
-public class ItensPersistencia implements ItensNecessidade, ItensParaONGSolicitar {
-
-}
